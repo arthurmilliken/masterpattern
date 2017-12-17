3274a80d3d791ccd5223c80170c4c75128f0b867
Arthur Milliken <amilliken@gmail.com>
2017-12-17 09:42:25 +0000

    UPDATE: true and invisible

--- routes/keys.js
+++ routes/keys.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const tarot = require('../models/tarot');
 
 module.exports = function (router) {
@@ -33,11 +34,18 @@ module.exports = function (router) {
 
     // Substitute final letters where needed.
     hebrew = hebrew
-    .replace(/כ[ ]/g, 'ך ')
-    .replace(/מ[ ]/g, 'ם ')
-    .replace(/נ[ ]/g, 'ן ')
-    .replace(/פ[ ]/g, 'ף ')
-    .replace(/צ[ ]/g, 'ץ ')
+    .replace(/כ /g, 'ך ')
+    .replace(/מ /g, 'ם ')
+    .replace(/נ /g, 'ן ')
+    .replace(/פ /g, 'ף ')
+    .replace(/צ /g, 'ץ ')
+
+    .replace(/כ־/g, 'ך־')
+    .replace(/מ־/g, 'ם־')
+    .replace(/נ־/g, 'ן־')
+    .replace(/פ־/g, 'ף־')
+    .replace(/צ־/g, 'ץ־')
+
     .replace(/^ך/, 'כ')
     .replace(/^ם/, 'מ')
     .replace(/^ן/, 'נ')
@@ -52,17 +60,21 @@ module.exports = function (router) {
     .replace(/_/g, ' ')
     ;
 
+    let longest = _.reduce(hebrew.split(' '), (sum, word) => {
+      return Math.max(word.length, sum);
+    }, 0);
+
     let letterWidth = 200;
     let letterHeight = 323;
-    if (letters.length > 22) {
+    if (longest > 11) {
       letterWidth = 48;
       letterHeight = 77;
     }
-    else if (letters.length > 11) {
+    else if (longest > 7) {
       letterWidth = 77;
       letterHeight = 124;
     }
-    else if (letters.length > 4) {
+    else if (longest > 4) {
       letterWidth = 124;
       letterHeight = 200;
     }
