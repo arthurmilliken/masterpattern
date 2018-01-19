d7ce573af20660a8156deb981bd20835e10cda2e
Arthur Milliken <amilliken@gmail.com>
2018-01-19 18:57:53 +0000

    add switch-deck script

---
+++ switch-deck.js
@@ -0,0 +1,31 @@
+const rimraf = require('rimraf');
+const ncp = require('ncp');
+
+let deck = process.argv[2];
+let path = `${__dirname}/secure/img/323x522`;
+let path_from = path;
+
+switch (deck) {
+  case 'bw':
+    path_from += '_bw';
+    break;
+  case 'new':
+    path_from += '_new';
+    break;
+  case 'orig':
+    path_from += '_orig';
+    break;
+  default:
+    console.log(`usage: node ${process.argv[1]} [bw | new | orig]`);
+    process.exit(1);
+    break;
+}
+
+rimraf(path, (err) => {
+  if (err) return console.error(err);
+  ncp(path_from, path, (err) => {
+    if (err) return console.error(err);
+    console.log(`copied ${path_from} to ${path}.`);
+  })
+});
+
