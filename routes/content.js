292bf31c130fb21698f18de1ca16be0d420132f6
Arthur Milliken <amilliken@gmail.com>
2018-04-23 21:30:52 -0700

    genesis

--- routes/content.js
+++ routes/content.js
@@ -5,30 +5,46 @@ const path = require('path');
 
 const contentDir = path.resolve(__dirname, '../secure/content');
 
+function renderContent(entry, name, res) {
+  co(function* () {
+    const file = path.resolve(contentDir, `${entry}.md`);
+    try {
+      const content = yield fs.readFile(file, 'utf8');
+      res.locals.content = markdown.toHTML(content);
+    } catch (e) {
+      res.locals.content = `No content found for ${name}`;
+    }
+    res.locals.title = name;
+    res.render('content');
+  });
+}
+
 module.exports = function (router) {
   router.get('/content/:entry', (req, res) => {
-    co(function* () {
-      let entry = req.params.entry.trim();
-      entry = entry.replace(/.md$/, '');
-      const num = parseInt(entry, 10);
-      if (num < 10) {
-        entry = `0-9/${entry}`;
-      } else if (num < 100) {
-        entry = `10-99/${entry}`;
-      } else if (num < 1000) {
-        entry = `100-999/${entry}`;
-      } else if (num < 10000) {
-        entry = `1000-9999/${entry}`;
-      }
-      const file = path.resolve(contentDir, `${entry}.md`);
-      try {
-        const content = yield fs.readFile(file, 'utf8');
-        res.locals.content = markdown.toHTML(content);
-      } catch (e) {
-        res.locals.content = `No content found for ${entry}`;
-      }
-      res.locals.title = `Gematria - ${entry}`;
-      res.render('content');
-    });
+    let entry = req.params.entry.trim();
+    entry = entry.replace(/.md$/, '');
+    const name = entry;
+    const num = parseInt(entry, 10);
+    if (num < 10) {
+      entry = `0-9/${entry}`;
+    } else if (num < 100) {
+      entry = `10-99/${entry}`;
+    } else if (num < 1000) {
+      entry = `100-999/${entry}`;
+    } else if (num < 10000) {
+      entry = `1000-9999/${entry}`;
+    }
+    renderContent(entry, name, res);
+  });
+
+  router.get('/genesis/:chapter', (req, res) => {
+    const chapter = req.params.chapter;
+    renderContent(`genesis/${chapter}`, `Genesis ${chapter}`, res);
+  });
+
+  router.get('/genesis/:chapter/:verse', (req, res) => {
+    const chapter = req.params.chapter;
+    const verse = req.params.verse;
+    renderContent(`genesis/${chapter}/${verse}`, `Genesis ${chapter}:${verse}`, res);
   });
 };
